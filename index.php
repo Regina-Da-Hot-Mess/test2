fa7b3fb7d6ff184b5f5fb62dafab162f5083da3b
Regina-Da-Hot-Mess <99183962+Regina-Da-Hot-Mess@users.noreply.github.com>
2022-02-09 17:04:31 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Hello</title>
+</head>
+<body>
+   
+    <p>And they were roommates</p>
+    
+
+    <?php
+    echo"<select>";
+        for ($y=1990; $y <=2022 ; $y++) { 
+            echo " <option>$y</option>";
+        }
+    echo "</select>";
+
+    echo "<p>I am gonna die</p>";
+    ?>
+
+
+<a href="new\roomates.php">Go to next</a>
+</body>
+</html>
